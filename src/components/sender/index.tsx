d2974cb85635fbca765ab9f03d5ba16fa5a1433e
Tanishq Singh <tanishqsingh640@gmail.com>
2022-08-29 04:54:18 +0000

    Minor bug fixed

--- src/components/sender/index.tsx
+++ src/components/sender/index.tsx
@@ -1,108 +1,122 @@
 import { useContext, useEffect, useState } from "react";
-import { WebSocketCtx } from '@/context/websocket';
-import peerConfig from '@/utils/peerConfig';
+import { WebSocketCtx } from "@/context/websocket";
+import peerConfig from "@/utils/peerConfig";
 
 interface SocketMessage {
-    type?: string;
-    sdp?: string;
-    myId?: string;
-    candidate?: string;
+	type?: string;
+	sdp?: string;
+	myId?: string;
+	candidate?: string;
 }
 
 const Sender = () => {
-    const { uuid, ws, send } = useContext(WebSocketCtx);
-    const [online, setOnline] = useState<boolean>(false);
+	const { uuid, ws, send } = useContext(WebSocketCtx);
+	const [online, setOnline] = useState<boolean>(false);
 
-    useEffect(() => {
-        let pc = new RTCPeerConnection(peerConfig);
-        let dc = pc.createDataChannel("data-channel");
+	useEffect(() => {
+		let pc = new RTCPeerConnection(peerConfig);
+		let dc = pc.createDataChannel("data-channel");
 
-        (window as any).send = (data: string) => dc?.send(data);
-        (window as any).ws = ws;
+		(window as any).send = (data: string) => dc?.send(data);
+		(window as any).ws = ws;
 
-        dc.addEventListener("close", () => {
-            console.log("Closed");
-            setOnline(false);
-        });
+		dc.addEventListener("close", () => {
+			console.log("Closed");
+			setOnline(false);
+		});
 
-        dc.addEventListener("message", ({ data }) => console.log(data));
+		dc.addEventListener("message", ({ data }) => console.log(data));
 
-        dc.addEventListener("open", () => {
-            ws?.close();
-            setOnline(true);
-            console.log("Opened");
-        });
+		dc.addEventListener("open", () => {
+			ws?.close();
+			setOnline(true);
+			console.log("Opened");
+		});
 
-        pc.addEventListener("connectionstatechange", () => console.warn(pc.connectionState));
+		pc.addEventListener("connectionstatechange", () =>
+			console.warn(pc.connectionState)
+		);
 
-        if (!ws) return;
+		if (!ws) return;
 
-        ws.onmessage = async ({ data: rawData }) => {
-            if (typeof rawData !== "string") return;
-            if (rawData.length === 0) return;
+		ws.onmessage = async ({ data: rawData }) => {
+			if (typeof rawData !== "string") return;
+			if (rawData.length === 0) return;
 
-            try {
-                const { type, myId, sdp, candidate } = (JSON.parse(rawData)?.message as SocketMessage);
+			try {
+				const { type } = JSON.parse(rawData);
 
-                if (!type || !myId) return;
+				if (type === "sendto") {
+					const { type, myId, sdp, candidate } = JSON.parse(rawData)?.message as SocketMessage;
 
-                pc.addEventListener("icecandidate", ({ candidate }) => candidate && send({
-                    type: "sendto",
-                    sendTo: myId,
-                    message: {
-                        type: "candidate",
-                        candidate: JSON.stringify(candidate),
-                        myId: uuid
-                    }
-                }));
+					if (!type || !myId) return;
 
-                switch (type) {
-                    case "can-i-get-a-offer":
-                        await pc.createOffer()
-                            .then(async (offer) => {
-                                await pc.setLocalDescription(offer);
+					pc.addEventListener(
+						"icecandidate",
+						({ candidate }) =>
+							candidate &&
+							send({
+								type: "sendto",
+								sendTo: myId,
+								message: {
+									type: "candidate",
+									candidate: JSON.stringify(candidate),
+									myId: uuid,
+								},
+							})
+					);
 
-                                send({
-                                    type: "sendto",
-                                    sendTo: myId,
-                                    message: {
-                                        type: "offer",
-                                        sdp: JSON.stringify(offer),
-                                        myId: uuid
-                                    }
-                                });
-                            });
+					switch (type) {
+						case "can-i-get-a-offer":
+							await pc.createOffer().then(async (offer) => {
+								await pc.setLocalDescription(offer);
 
-                        break;
+								send({
+									type: "sendto",
+									sendTo: myId,
+									message: {
+										type: "offer",
+										sdp: JSON.stringify(offer),
+										myId: uuid,
+									},
+								});
+							});
 
-                    case "answer":
-                        if (sdp)
-                            await pc.setRemoteDescription(JSON.parse(sdp));
+							break;
 
-                        break;
+						case "answer":
+							if (sdp)
+								await pc.setRemoteDescription(JSON.parse(sdp));
 
-                    case "candidate":
-                        if (candidate)
-                            await pc.addIceCandidate(JSON.parse(candidate));
+							break;
 
-                        break;
+						case "candidate":
+							if (candidate)
+								await pc.addIceCandidate(JSON.parse(candidate));
 
-                    default:
-                        break;
+							break;
+
+						default:
+							break;
+					}
+				}
+
+                else if (type === "pong"){
+                    console.warn("Maintaining ws")
                 }
-            } catch (error) {
-                console.error((error as Error));
-            }
-        }
+			} catch (error) {
+				console.error(error as Error);
+			}
+		};
 
-        // eslint-disable-next-line
-    }, []);
+		// eslint-disable-next-line
+	}, []);
 
-    return (
-        <div>
-            {online ? <span>Connected</span> : <span>Not connected</span>}
-        </div>
-    )
-}
+	return (
+		<div>
+			{online ? <span>Connected</span> : <span>Not connected</span>}
+		</div>
+	);
+};
 
 export default Sender;
--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,119 +1,131 @@
-import { useEffect, useContext, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { WebSocketCtx } from '@/context/websocket';
-import peerConfig from '@/utils/peerConfig';
+import { useEffect, useContext, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { WebSocketCtx } from "@/context/websocket";
+import peerConfig from "@/utils/peerConfig";
 
 interface SocketMessage {
-    type?: string;
-    sdp?: string;
-    myId?: string;
-    candidate?: string;
+	type?: string;
+	sdp?: string;
+	myId?: string;
+	candidate?: string;
 }
 
 const DownloadPage = () => {
-    const { pathname } = useLocation();
-    const { send, uuid: id, ws } = useContext(WebSocketCtx);
-    const [online, setOnline] = useState<boolean>(false);
+	const { pathname } = useLocation();
+	const { send, uuid: id, ws } = useContext(WebSocketCtx);
+	const [online, setOnline] = useState<boolean>(false);
 
-    useEffect(() => {
-        const uuid: string = pathname.split("/")[1];
-        let pc = new RTCPeerConnection(peerConfig);
-        let dc: RTCDataChannel | null;
+	useEffect(() => {
+		const uuid: string = pathname.split("/")[1];
+		let pc = new RTCPeerConnection(peerConfig);
+		let dc: RTCDataChannel | null;
 
-        pc.addEventListener("datachannel", ({ channel }) => {
-            dc = channel;
+		pc.addEventListener("datachannel", ({ channel }) => {
+			dc = channel;
 
-            (window as any).send = (data: string) => dc?.send(data);
-            (window as any).ws = ws;
+			(window as any).send = (data: string) => dc?.send(data);
+			(window as any).ws = ws;
 
-            dc.addEventListener("message", ({ data }) => console.log(data));
-            dc.addEventListener("close", () => {
-                console.log("Closed");
-                setOnline(false);
-            });
-            dc.addEventListener("open", () => {
-                ws?.close();
-                console.log("Opened");
-                setOnline(true)
-            });
-        });
+			dc.addEventListener("message", ({ data }) => console.log(data));
+			dc.addEventListener("close", () => {
+				console.log("Closed");
+				setOnline(false);
+			});
+			dc.addEventListener("open", () => {
+				ws?.close();
+				console.log("Opened");
+				setOnline(true);
+			});
+		});
 
-        pc.addEventListener("connectionstatechange", () => console.warn(pc.connectionState));
+		pc.addEventListener("connectionstatechange", () =>
+			console.warn(pc.connectionState)
+		);
 
-        send({
-            type: "sendto",
-            sendTo: uuid,
-            message: {
-                type: "can-i-get-a-offer",
-                sdp: "",
-                myId: id
-            }
-        });
+		send({
+			type: "sendto",
+			sendTo: uuid,
+			message: {
+				type: "can-i-get-a-offer",
+				sdp: "",
+				myId: id,
+			},
+		});
 
-        if (!ws) return;
+		if (!ws) return;
 
-        ws.onmessage = async ({ data: rawData }) => {
-            if (typeof rawData !== "string") return;
-            if (rawData.length === 0) return;
+		ws.onmessage = async ({ data: rawData }) => {
+			if (typeof rawData !== "string") return;
+			if (rawData.length === 0) return;
 
-            try {
-                const { type, myId, sdp, candidate } = (JSON.parse(rawData)?.message as SocketMessage);
+			try {
+				const { type } = JSON.parse(rawData);
 
-                if (!type || !myId) return;
+				if (type === "sendto") {
+					const { type, myId, sdp, candidate } = JSON.parse(rawData)?.message as SocketMessage;
 
-                pc.addEventListener("icecandidate", ({ candidate }) => candidate && send({
-                    type: "sendto",
-                    sendTo: uuid,
-                    message: {
-                        type: "candidate",
-                        candidate: JSON.stringify(candidate),
-                        myId: id
-                    }
-                }));
+					if (!type || !myId) return;
 
-                switch (type) {
-                    case "offer":
-                        if (!sdp) return;
-                        await pc.setRemoteDescription(JSON.parse(sdp));
+					pc.addEventListener(
+						"icecandidate",
+						({ candidate }) =>
+							candidate &&
+							send({
+								type: "sendto",
+								sendTo: uuid,
+								message: {
+									type: "candidate",
+									candidate: JSON.stringify(candidate),
+									myId: id,
+								},
+							})
+					);
 
-                        await pc.createAnswer()
-                            .then(async (answer) => {
-                                await pc.setLocalDescription(answer);
+					switch (type) {
+						case "offer":
+							if (!sdp) return;
+							await pc.setRemoteDescription(JSON.parse(sdp));
 
-                                send({
-                                    type: "sendto",
-                                    sendTo: uuid,
-                                    message: {
-                                        type: "answer",
-                                        sdp: JSON.stringify(answer),
-                                        myId: id
-                                    }
-                                });
-                            });
-                        break;
+							await pc.createAnswer().then(async (answer) => {
+								await pc.setLocalDescription(answer);
 
-                    case "candidate":
-                        if (candidate)
-                            await pc.addIceCandidate(JSON.parse(candidate));
+								send({
+									type: "sendto",
+									sendTo: uuid,
+									message: {
+										type: "answer",
+										sdp: JSON.stringify(answer),
+										myId: id,
+									},
+								});
+							});
+							break;
 
-                        break;
+						case "candidate":
+							if (candidate)
+								await pc.addIceCandidate(JSON.parse(candidate));
 
-                    default:
-                        break;
-                }
-            } catch (error) {
-                console.error((error as Error))
-            }
-        }
+							break;
 
-        // eslint-disable-next-line
-    }, []);
+						default:
+							break;
+					}
+				} else if (type === "pong") {
+					console.warn("Maintaining ws");
+				}
+			} catch (error) {
+				console.error(error as Error);
+			}
+		};
 
-    return (
-        <div>
-            {online ? <span>Connected</span> : <span>Not connected</span>}
-        </div>
-    )
-}
+		// eslint-disable-next-line
+	}, []);
+
+	return (
+		<div>
+			{online ? <span>Connected</span> : <span>Not connected</span>}
+		</div>
+	);
+};
 
 export default DownloadPage;
