780202ffc20588b11e07b68d2c634fdaf4f64ca5
Tanishq Singh <76192403+tanishq-singh-2301@users.noreply.github.com>
2022-08-29 03:19:53 +0000

    trying with metered.ca turn/stun server

--- src/utils/peerConfig.ts
+++ src/utils/peerConfig.ts
@@ -6,6 +6,24 @@ const configuration = {
                 'stun:stun2.l.google.com:19302',
             ],
         },
+        {
+            urls: ["stun:openrelay.metered.ca:80"],
+        },
+        {
+            urls: ["turn:openrelay.metered.ca:80"],
+            username: "openrelayproject",
+            credential: "openrelayproject",
+        },
+        {
+            urls: ["turn:openrelay.metered.ca:443"],
+            username: "openrelayproject",
+            credential: "openrelayproject",
+        },
+        {
+            urls: ["turn:openrelay.metered.ca:443?transport=tcp"],
+            username: "openrelayproject",
+            credential: "openrelayproject",
+        },
     ],
     iceCandidatePoolSize: 10,
 };
