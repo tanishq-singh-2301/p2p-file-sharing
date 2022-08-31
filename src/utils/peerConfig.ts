50f35d862781eb360f33475cc36d7f840f92ffe2
Tanishq Singh <tanishqsingh640@gmail.com>
2022-08-31 13:05:01 +0000

    added more stun and turn server for connection

--- src/utils/peerConfig.ts
+++ src/utils/peerConfig.ts
@@ -2,8 +2,8 @@ const configuration: RTCConfiguration = {
 	iceServers: [
 		{
 			urls: [
-				// "stun:stun1.l.google.com:19302",
-				// "stun:stun2.l.google.com:19302",
+				"stun:stun1.l.google.com:19302",
+				"stun:stun2.l.google.com:19302",
 				"stun:openrelay.metered.ca:80",
 			],
 		},
@@ -12,11 +12,11 @@ const configuration: RTCConfiguration = {
 			username: "openrelayproject",
 			credential: "openrelayproject",
 		},
-		// {
-		//     urls: ["turn:openrelay.metered.ca:443"],
-		//     username: "openrelayproject",
-		//     credential: "openrelayproject",
-		// }
+		{
+		    urls: ["turn:openrelay.metered.ca:443"],
+		    username: "openrelayproject",
+		    credential: "openrelayproject",
+		}
 	],
 	iceCandidatePoolSize: 10,
 };
