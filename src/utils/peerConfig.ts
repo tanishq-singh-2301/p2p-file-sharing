6005e2893909c92544dd86fd1b84cea701f13229
Tanishq Singh <76192403+tanishq-singh-2301@users.noreply.github.com>
2022-09-01 08:45:21 +0530

    Update peerConfig.ts

--- src/utils/peerConfig.ts
+++ src/utils/peerConfig.ts
@@ -2,21 +2,21 @@ const configuration: RTCConfiguration = {
 	iceServers: [
 		{
 			urls: [
-				// "stun:stun1.l.google.com:19302",
-				// "stun:stun2.l.google.com:19302",
+				"stun:stun.l.google.com:19302",
 				"stun:openrelay.metered.ca:80",
+                "stun:global.stun.twilio.com:3478"
 			],
 		},
-		{
-			urls: ["turn:openrelay.metered.ca:80"],
-			username: "openrelayproject",
-			credential: "openrelayproject",
-		},
-		// {
-		//     urls: ["turn:openrelay.metered.ca:443"],
-		//     username: "openrelayproject",
-		//     credential: "openrelayproject",
-		// }
+// 		{
+// 			urls: ["turn:openrelay.metered.ca:80"],
+// 			username: "openrelayproject",
+// 			credential: "openrelayproject",
+// 		},
+// 		{
+// 		    urls: ["turn:openrelay.metered.ca:443"],
+// 		    username: "openrelayproject",
+// 		    credential: "openrelayproject",
+// 		}
 	]
 };
 
